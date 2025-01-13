8fe55886bc671fbb41c81243c30799fff809390d
CharminDoge <danielechianella@hotmail.it>
2025-01-13 23:15:30 +0100

    added cvss calculation with go-cvss

--- app/internal/cvss/parse.go
+++ app/internal/cvss/parse.go
@@ -1,5 +1,41 @@
 package cvss
 
-func ParseVector(vector string) (float32, error) {
-	return 8.1, nil
+import (
+	"errors"
+
+	gocvss20 "github.com/pandatix/go-cvss/20"
+	gocvss30 "github.com/pandatix/go-cvss/30"
+	gocvss31 "github.com/pandatix/go-cvss/31"
+	gocvss40 "github.com/pandatix/go-cvss/40"
+)
+
+func ParseVector(vector string, version int) (float64, error) {
+	switch version {
+	case CVSS2:
+		cvss, err := gocvss20.ParseVector(vector)
+		if err != nil {
+			return 0, err
+		}
+		return cvss.EnvironmentalScore(), nil
+	case CVSS3:
+		cvss, err := gocvss30.ParseVector(vector)
+		if err != nil {
+			return 0, err
+		}
+		return cvss.EnvironmentalScore(), nil
+	case CVSS31:
+		cvss, err := gocvss31.ParseVector(vector)
+		if err != nil {
+			return 0, err
+		}
+		return cvss.EnvironmentalScore(), nil
+	case CVSS4:
+		cvss, err := gocvss40.ParseVector(vector)
+		if err != nil {
+			return 0, err
+		}
+		return cvss.Score(), nil
+	default:
+		return 0, errors.New("Invalid CVSS version")
+	}
 }
