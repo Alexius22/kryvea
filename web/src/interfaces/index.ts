6869e99ec67d8212fdcdf3e061b35ba37ee79d2f
Alexius22 <amatucci.work@gmail.com>
2025-01-18 01:14:31 +0100

    Minor changes

--- web/src/interfaces/index.ts
+++ web/src/interfaces/index.ts
@@ -1,8 +1,3 @@
-export type UserPayloadObject = {
-  name: string
-  email: string
-}
-
 export type MenuAsideItem = {
   label: string
   icon?: string
@@ -40,32 +35,6 @@ export type ColorButtonKey =
   | 'void'
 
 export type BgKey = 'purplePink' | 'pinkRed'
-
-export type TrendType = 'up' | 'down' | 'success' | 'danger' | 'warning' | 'info'
-
-export type TransactionType = 'withdraw' | 'deposit' | 'invoice' | 'payment'
-
-export type Transaction = {
-  id: number
-  amount: number
-  account: string
-  name: string
-  date: string
-  type: TransactionType
-  business: string
-}
-
-export type Client = {
-  id: number
-  avatar: string
-  login: string
-  name: string
-  company: string
-  city: string
-  progress: number
-  created: string
-  created_mm_dd_yyyy: string
-}
 
 export type UserForm = {
   name: string
