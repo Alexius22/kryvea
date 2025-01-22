d6036cb749ba31ea81e969489a2608f9f617045d
JJJJJJack <97jack@live.it>
2025-01-22 01:48:07 +0100

    REF: Refactor CVSSCalculator3 to use buttons for input and add ScoreBar component; update AccordionItem to use title prop

--- web/src/components/CVSS/CVSSCalculator3.tsx
+++ web/src/components/CVSS/CVSSCalculator3.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Buttons from "../Buttons";
+import Button from "../Button";
+import ScoreBar from "./ScoreBar";
 
 const CVSSCalculator3 = ({ onChange, onSubmit }) => {
   const [selectedValues, setSelectedValues] = useState({
@@ -61,38 +64,6 @@ const CVSSCalculator3 = ({ onChange, onSubmit }) => {
     }
   };
 
-  const renderDropdown = () => {
-    return (
-      <div className="cvss-dropdown">
-        <form onSubmit={handleSubmit}>
-          {Object.keys(bg).map(group => (
-            <div key={group}>
-              <h3>{bg[group]}</h3>
-              {Object.keys(bm[group]).map(value => (
-                <label key={value}>
-                  <input
-                    type="radio"
-                    name={group}
-                    value={value}
-                    checked={selectedValues[group] === value}
-                    onChange={() => handleChange(group, value)}
-                  />
-                  {bm[group][value]}
-                </label>
-              ))}
-            </div>
-          ))}
-          <div className="submit-section">
-            <button type="submit">Submit</button>
-            <button type="button" onClick={handleModifyDropdown}>
-              Modify Selections
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  };
-
   const renderResults = () => {
     const cvssScore = calculateCVSS(selectedValues);
     return (
@@ -113,7 +84,60 @@ const CVSSCalculator3 = ({ onChange, onSubmit }) => {
     return `CVSS:3.1/AV:${selectedValues.AV}/AC:${selectedValues.AC}/PR:${selectedValues.PR}/UI:${selectedValues.UI}/S:${selectedValues.S}/C:${selectedValues.C}/I:${selectedValues.I}/A:${selectedValues.A}`;
   };
 
-  return <div>{dropdownOpen ? renderDropdown() : renderResults()}</div>;
+  return (
+    <div>
+      <p className="mb-2 font-bold">Attack Vector (AV)</p>
+      <Buttons>
+        <Button color="contrast" label="Network (N)" onClick={() => handleChange("AV", "N")} />
+        <Button color="contrast" label="Adjacent (A)" onClick={() => handleChange("AV", "A")} />
+        <Button color="contrast" label="Local (L)" onClick={() => handleChange("AV", "L")} />
+        <Button color="contrast" label="Physical (P)" onClick={() => handleChange("AV", "P")} />
+      </Buttons>
+      <p className="my-2 font-bold">Attack Complexity (AC)</p>
+      <Buttons>
+        <Button color="contrast" label="Low (L)" onClick={() => handleChange("AC", "L")} />
+        <Button color="contrast" label="High (H)" onClick={() => handleChange("AC", "H")} />
+      </Buttons>
+      <p className="my-2 font-bold">Privileges Required (PR)</p>
+      <Buttons>
+        <Button color="contrast" label="None (N)" onClick={() => handleChange("PR", "N")} />
+        <Button color="contrast" label="Low (L)" onClick={() => handleChange("PR", "L")} />
+        <Button color="contrast" label="High (H)" onClick={() => handleChange("PR", "H")} />
+      </Buttons>
+      <p className="my-2 font-bold">User Interaction (UI)</p>
+      <Buttons>
+        <Button color="contrast" label="None (N)" onClick={() => handleChange("UI", "N")} />
+        <Button color="contrast" label="Required (R)" onClick={() => handleChange("UI", "R")} />
+      </Buttons>
+      <p className="my-2 font-bold">Scope (S)</p>
+      <Buttons>
+        <Button color="contrast" label="Unchanged (U)" onClick={() => handleChange("S", "U")} />
+        <Button color="contrast" label="Changed (C)" onClick={() => handleChange("S", "C")} />
+      </Buttons>
+      <p className="my-2 font-bold">Confidentiality (C)</p>
+      <Buttons>
+        <Button color="contrast" label="None (N)" onClick={() => handleChange("C", "N")} />
+        <Button color="contrast" label="Low (L)" onClick={() => handleChange("C", "L")} />
+        <Button color="contrast" label="High (H)" onClick={() => handleChange("C", "H")} />
+      </Buttons>
+      <p className="my-2 font-bold">Integrity (I)</p>
+      <Buttons>
+        <Button color="contrast" label="None (N)" onClick={() => handleChange("I", "N")} />
+        <Button color="contrast" label="Low (L)" onClick={() => handleChange("I", "L")} />
+        <Button color="contrast" label="High (H)" onClick={() => handleChange("I", "H")} />
+      </Buttons>
+      <p className="my-2 font-bold">Availability (A)</p>
+      <Buttons>
+        <Button color="contrast" label="None (N)" onClick={() => handleChange("A", "N")} />
+        <Button color="contrast" label="Low (L)" onClick={() => handleChange("A", "L")} />
+        <Button color="contrast" label="High (H)" onClick={() => handleChange("A", "H")} />
+      </Buttons>
+      <p className="my-2 font-bold">
+        Score: <ScoreBar score={1} />
+      </p>
+      <p className="my-2 font-bold">Severity: </p>
+    </div>
+  );
 };
 
 export default CVSSCalculator3;
