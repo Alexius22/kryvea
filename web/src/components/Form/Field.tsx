77783d8a0991890fbc4cdb90a45ad4515b685f4e
Alexius22 <amatucci.work@gmail.com>
2025-01-26 23:01:11 +0100

    Added error handling to FormField and integrated with AddVulnerability component

--- web/src/components/Form/Field.tsx
+++ web/src/components/Form/Field.tsx
@@ -8,13 +8,14 @@ type Props = {
   icons?: string[] | null[];
   isBorderless?: boolean;
   isTransparent?: boolean;
+  isError?: boolean;
   hasTextareaHeight?: boolean;
   noHeight?: boolean;
   gridTemplateColumns?: string;
   children: ReactNode;
 };
 
-const FormField = ({ icons = [], noHeight, gridTemplateColumns, ...props }: Props) => {
+const FormField = ({ icons = [], noHeight, gridTemplateColumns, isError = false, ...props }: Props) => {
   const childrenCount = Children.count(props.children);
 
   const elementWrapperClass = gridTemplateColumns
@@ -25,14 +26,6 @@ const FormField = ({ icons = [], noHeight, gridTemplateColumns, ...props }: Prop
         ? "grid grid-cols-1 gap-3 md:grid-cols-3"
         : "grid grid-cols-1 gap-3";
 
-  const controlClassName = [
-    "px-3 py-2 max-w-full border-gray-700 rounded w-full dark:placeholder-gray-400",
-    "focus:ring focus:ring-blue-600 focus:border-blue-600 focus:outline-none",
-    props.hasTextareaHeight ? "h-48" : "h-12",
-    props.isBorderless ? "border-0" : "border",
-    props.isTransparent ? "bg-transparent" : "bg-white dark:bg-slate-800",
-  ].join(" ");
-
   const labels = Array.isArray(props.label) ? props.label : [props.label];
 
   return (
@@ -40,33 +33,51 @@ const FormField = ({ icons = [], noHeight, gridTemplateColumns, ...props }: Prop
       <div className={elementWrapperClass}>
         {Children.toArray(props.children)
           .filter((child): child is ReactElement => React.isValidElement(child))
-          .map((child, index) => (
-            <div className="relative" key={index}>
-              {labels[index] ? (
-                <label
-                  htmlFor={props.labelFor}
-                  className={`mb-2 block font-bold ${props.labelFor ? "cursor-pointer" : ""}`}
-                >
-                  {labels[index]}
-                </label>
-              ) : (
-                <div className={`mb-2 block font-bold ${noHeight ? "" : "h-6"}`} />
-              )}
-              {cloneElement(child, {
-                className: `${controlClassName} ${icons[index] ? "pl-10" : ""}`,
-              })}
-              {icons[index] && (
-                <Icon
-                  path={icons[index]}
-                  w="w-10"
-                  h={props.hasTextareaHeight ? "h-full" : "h-12"}
-                  className="pointer-events-none absolute left-0 top-8 z-10 text-gray-500 dark:text-slate-400"
-                />
-              )}
-            </div>
-          ))}
+          .map((child, index) => {
+            const isError = child.props.isError;
+            const controlClassName = [
+              "px-3 py-2 max-w-full rounded w-full dark:placeholder-gray-400",
+              "focus:ring focus:outline-none",
+              isError
+                ? "border-red-500 focus:ring-red-500 focus:border-red-500"
+                : "border-gray-700 focus:ring-blue-600 focus:border-blue-600",
+              props.hasTextareaHeight ? "h-48" : "h-12",
+              props.isBorderless ? "border-0" : "border",
+              props.isTransparent ? "bg-transparent" : "bg-white dark:bg-slate-800",
+            ].join(" ");
+
+            return (
+              <div className="relative" key={index}>
+                {labels[index] ? (
+                  <label
+                    htmlFor={props.labelFor}
+                    className={`mb-2 block font-bold ${props.labelFor ? "cursor-pointer" : ""}`}
+                  >
+                    {labels[index]}
+                  </label>
+                ) : (
+                  <div className={`mb-2 block font-bold ${noHeight ? "" : "h-6"}`} />
+                )}
+                {cloneElement(child, {
+                  className: `${controlClassName} ${icons[index] ? "pl-10" : ""}`,
+                })}
+                {icons[index] && (
+                  <Icon
+                    path={icons[index]}
+                    w="w-10"
+                    h={props.hasTextareaHeight ? "h-full" : "h-12"}
+                    className="pointer-events-none absolute left-0 top-8 z-10 text-gray-500 dark:text-slate-400"
+                  />
+                )}
+              </div>
+            );
+          })}
       </div>
-      {props.help && <div className="mt-1 text-xs text-gray-500 dark:text-slate-400">{props.help}</div>}
+      {props.help && (
+        <div className={`mt-1 text-xs ${isError ? "text-red-500" : "text-gray-500 dark:text-slate-400"}`}>
+          {props.help}
+        </div>
+      )}
     </div>
   );
 };
