628f6e02547e0f7a8334961272661608c7b17244
JJJJJJack <97jack@live.it>
2025-01-28 01:29:34 +0100

    ADD: CardBox wrapper for styling table & search bar logic

--- web/src/components/Table/Table.tsx
+++ web/src/components/Table/Table.tsx
@@ -1,16 +1,32 @@
 import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
 import { Field, Form, Formik } from "formik";
-import { isValidElement, useState } from "react";
+import { isValidElement, useEffect, useState } from "react";
 import Button from "../Button";
 import Buttons from "../Buttons";
 import FormField from "../Form/Field";
 import Icon from "../Icon/Icon";
+import CardBox from "../CardBox";
 
-const Table = ({ data, buttons, perPageCustom }: { data; buttons?; perPageCustom }) => {
-  const [originalData] = useState([...data]);
+const Table = ({ data, buttons, perPageCustom }: { data: any[]; buttons?; perPageCustom }) => {
   const [perPage, setPerPage] = useState(perPageCustom);
   const [currentPage, setCurrentPage] = useState(0);
   const [keySort, setKeySort] = useState<{ header: string; order: 1 | 2 }>();
+  const [filterText, setFilterText] = useState("");
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    setCurrentPage(0);
+    setFilteredData(
+      data.filter(obj => {
+        return Object.values(obj).some(value => {
+          if (isValidElement(value)) {
+            value = (value as any).props.children;
+          }
+          return value.toString().toLowerCase().includes(filterText.toLowerCase());
+        });
+      })
+    );
+  }, [filterText]);
 
   const sortAscend = (a, b) => {
     a = a[keySort.header];
@@ -44,14 +60,16 @@ const Table = ({ data, buttons, perPageCustom }: { data; buttons?; perPageCustom
         arr = arr.sort(sortDescend);
         break;
       case undefined:
-        arr = originalData; // reset to original order
+        arr = filteredData; // reset to original order
         break;
       default:
         console.warn("keySort unknown value = ", keySort);
     }
+
     return arr.slice(perPage * currentPage, perPage * (currentPage + 1));
   };
-  const numPages = Math.ceil(data.length / perPage);
+
+  const numPages = Math.ceil(filteredData.length / perPage);
   const pagesList = [];
 
   for (let i = 0; i < numPages; i++) {
@@ -72,79 +90,88 @@ const Table = ({ data, buttons, perPageCustom }: { data; buttons?; perPageCustom
 
   return (
     <>
-      <table className="w-full table-auto border-collapse">
-        <thead>
-          <tr>
-            {data.length === 0 ? (
-              <th>empty</th>
-            ) : (
-              Object.keys(data[0]).map((key, i, arr) => {
-                return (
-                  <th
-                    style={{
-                      width: `${100 / arr.length}%`,
-                    }}
-                    className="cursor-pointer align-middle hover:opacity-60"
-                    onClick={onHeaderClick(key)}
-                    key={`header-${key}`}
-                  >
-                    {key}
-                    <Icon
-                      className={keySort === undefined ? "opacity-0" : keySort.header !== key ? "opacity-0" : ""}
-                      h="h-min"
-                      w="w-min"
-                      path={keySort?.order === 1 ? mdiChevronDown : mdiChevronUp}
-                    />
-                  </th>
-                );
-              })
-            )}
-            {buttons == undefined ? <></> : <th className="w-min" />}
-          </tr>
-        </thead>
-        <tbody>
-          {data.length === 0 ? (
+      <input
+        className="mb-2 h-10 w-80 rounded-lg border-0 bg-slate-900/50"
+        placeholder="Search"
+        type="text"
+        value={filterText}
+        onChange={e => setFilterText(e.target.value)}
+      />
+      <CardBox hasTable>
+        <table className="w-full table-auto border-collapse">
+          <thead>
             <tr>
-              <td>empty</td>
+              {filteredData.length === 0 ? (
+                <th>empty</th>
+              ) : (
+                Object.keys(filteredData[0]).map((key, i, arr) => {
+                  return (
+                    <th
+                      style={{
+                        width: `${100 / arr.length}%`,
+                      }}
+                      className="cursor-pointer align-middle hover:opacity-60"
+                      onClick={onHeaderClick(key)}
+                      key={`header-${key}`}
+                    >
+                      {key}
+                      <Icon
+                        className={keySort === undefined ? "opacity-0" : keySort.header !== key ? "opacity-0" : ""}
+                        h="h-min"
+                        w="w-min"
+                        path={keySort?.order === 1 ? mdiChevronDown : mdiChevronUp}
+                      />
+                    </th>
+                  );
+                })
+              )}
+              {buttons == undefined ? <></> : <th className="w-min" />}
             </tr>
-          ) : (
-            itemPaginated(data).map((obj, i) => (
-              <tr key={`row-${i}`}>
-                {Object.entries<any>(obj).map(([key, value]) => (
-                  <td key={`${key}-value-${i}`}>{value}</td>
-                ))}
-                {buttons && buttons}
+          </thead>
+          <tbody>
+            {filteredData.length === 0 ? (
+              <tr>
+                <td>empty</td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
-      <div className="border-t border-gray-100 p-3 dark:border-slate-800 lg:px-6">
-        <div className="flex flex-col items-center justify-between py-3 md:flex-row md:py-0">
-          <Buttons>
-            {pagesList.map(page => (
-              <Button
-                key={page}
-                active={page === currentPage}
-                label={page + 1}
-                color={page === currentPage ? "lightDark" : "whiteDark"}
-                small
-                onClick={() => setCurrentPage(page)}
-              />
-            ))}
-          </Buttons>
-          <Formik initialValues={undefined} onSubmit={undefined}>
-            <Form className="ml-auto mr-2 w-20">
-              <FormField noHeight>
-                <Field type="number" value={perPage} onChange={val => setPerPage(+val.target.value)} />
-              </FormField>
-            </Form>
-          </Formik>
-          <small className="mt-6 md:mt-0">
-            Page {currentPage + 1} of {numPages}
-          </small>
+            ) : (
+              itemPaginated(filteredData).map((obj, i) => (
+                <tr key={`row-${i}`}>
+                  {Object.entries<any>(obj).map(([key, value]) => (
+                    <td key={`${key}-value-${i}`}>{value}</td>
+                  ))}
+                  {buttons && buttons}
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+        <div className="border-t border-gray-100 p-3 dark:border-slate-800 lg:px-6">
+          <div className="flex flex-col items-center justify-between py-3 md:flex-row md:py-0">
+            <Buttons>
+              {pagesList.map(page => (
+                <Button
+                  key={page}
+                  active={page === currentPage}
+                  label={page + 1}
+                  color={page === currentPage ? "lightDark" : "whiteDark"}
+                  small
+                  onClick={() => setCurrentPage(page)}
+                />
+              ))}
+            </Buttons>
+            <Formik initialValues={undefined} onSubmit={undefined}>
+              <Form className="ml-auto mr-2 w-20">
+                <FormField noHeight>
+                  <Field type="number" value={perPage} onChange={val => setPerPage(+val.target.value)} />
+                </FormField>
+              </Form>
+            </Formik>
+            <small className="mt-6 md:mt-0">
+              Page {currentPage + 1} of {numPages}
+            </small>
+          </div>
         </div>
-      </div>
+      </CardBox>
     </>
   );
 };
