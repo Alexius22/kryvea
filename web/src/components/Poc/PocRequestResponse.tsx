da6673a23dc13d594b376b5f2cf4bd5a538c12a4
JJJJJJack <97jack@live.it>
2025-01-30 00:41:31 +0100

    ADD: PocImage, PocRequestResponse, and PocText components for editing PoC details

---
+++ web/src/components/Poc/PocRequestResponse.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { PocDoc } from "../../pages/EditPoc";
+
+type PocProps = {
+  pocDoc: PocDoc;
+  i;
+  pocList: PocDoc[];
+  setPocList: React.Dispatch<React.SetStateAction<PocDoc[]>>;
+};
+
+export default function PocRequestResponse({ pocDoc, i, pocList, setPocList }: PocProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-4 grid">
+          <label htmlFor={`poc-title-${i}`}>Title</label>
+          <input
+            id={`poc-title-${i}`}
+            className="rounded dark:bg-slate-800"
+            value={pocDoc.title}
+            onChange={e =>
+              setPocList(prev => {
+                const newText = e.target.value;
+                const newPocList = [...prev];
+                newPocList[i] = { ...newPocList[i], title: newText };
+                return newPocList;
+              })
+            }
+          />
+        </div>
+        <div className="col-span-8 grid">
+          <label htmlFor={`poc-description-${i}`}>Description</label>
+          <textarea id={`poc-description-${i}`} className="rounded dark:bg-slate-800">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, recusandae?
+          </textarea>
+        </div>
+        <div className="col-span-full col-start-12 grid">
+          <label htmlFor={`poc-position-${i}`}>Position</label>
+          <input
+            id={`poc-position-${i}`}
+            type="number"
+            value={pocDoc.position}
+            min={0}
+            max={pocList.length - 1}
+            onChange={e => {
+              setPocList(prev => {
+                const positionNum = +e.target.value;
+                const newPocList = [...prev];
+                newPocList[i] = { ...newPocList[i], position: Math.min(positionNum, prev.length - 1) };
+                return newPocList;
+              });
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
