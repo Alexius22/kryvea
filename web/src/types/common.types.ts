58a39ea58418ceff811b6218b3e6b50f548f4b33
Alexius22 <amatucci.work@gmail.com>
2025-01-30 00:29:52 +0100

    Defined new types for Vulnerability, User, Assessment, Host, Customer, and Category

--- web/src/types/common.types.ts
+++ web/src/types/common.types.ts
@@ -1,1 +1,48 @@
 export type ObjectKey = string | number | symbol;
+
+export type Vulnerability = {
+    id: string;
+    detailed_title: string;
+    cvss_score: number;
+    cvss_vector: string;
+    assessment_id: number;
+    target_id: string;
+};
+
+export type User = {
+    id: string;
+    username: string;
+    role: string;
+    customers: string[];
+};
+
+export type Assessment = {
+    id: string;
+    name: string;
+    type: string;
+    start_date_time: string;
+    end_date_time: string;
+    status: string;
+};
+
+export type Host = {
+    id: string;
+    ip: string;
+    hostname: string;
+    customer_id: string;
+}
+
+export type Customer = {
+    id: string;
+    name: string;
+    language: string;
+    default_cvss_version: string;
+}
+
+export type Category = {
+    id: string;
+    index: string;
+    name: string;
+    generic_description: string;
+    generic_remediation: string;
+}
