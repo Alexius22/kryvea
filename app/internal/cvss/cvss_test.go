c04f345acc70c4c16b6bbe051d9ab531f2943df7
CharminDoge <danielechianella@hotmail.it>
2025-02-02 18:00:20 +0100

    Add CVSSSeverity field and update parsing logic for vulnerability assessment

--- app/internal/cvss/cvss_test.go
+++ app/internal/cvss/cvss_test.go
@@ -6,19 +6,21 @@ import (
 
 func TestParseVector(t *testing.T) {
 	tests := []struct {
-		name          string
-		vector        string
-		version       string
-		expectedScore float64
-		expectError   bool
+		name             string
+		vector           string
+		version          string
+		expectedScore    float64
+		expectedSeverity string
+		expectError      bool
 	}{
 		// Test cases for CVSS2
 		{
-			name:          "Valid CVSS2 vector",
-			vector:        "AV:L/AC:L/Au:S/C:P/I:P/A:C",
-			version:       CVSS2,
-			expectedScore: 5.7,
-			expectError:   false,
+			name:             "Valid CVSS2 vector",
+			vector:           "AV:L/AC:L/Au:S/C:P/I:P/A:C",
+			version:          CVSS2,
+			expectedScore:    5.7,
+			expectedSeverity: CVSS_MEDIUM,
+			expectError:      false,
 		},
 		{
 			name:        "Invalid CVSS2 vector",
@@ -29,11 +31,12 @@ func TestParseVector(t *testing.T) {
 
 		// Test cases for CVSS3
 		{
-			name:          "Valid CVSS3 vector",
-			vector:        "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
-			version:       CVSS3,
-			expectedScore: 9.8,
-			expectError:   false,
+			name:             "Valid CVSS3 vector",
+			vector:           "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
+			version:          CVSS3,
+			expectedScore:    9.8,
+			expectedSeverity: CVSS_CRITICAL,
+			expectError:      false,
 		},
 		{
 			name:        "Invalid CVSS3 vector",
@@ -44,11 +47,12 @@ func TestParseVector(t *testing.T) {
 
 		// Test cases for CVSS31
 		{
-			name:          "Valid CVSS3 vector",
-			vector:        "CVSS:3.1/AV:N/AC:H/PR:L/UI:R/S:U/C:L/I:L/A:L/E:P/RL:X/RC:X",
-			version:       CVSS31,
-			expectedScore: 4.4,
-			expectError:   false,
+			name:             "Valid CVSS3 vector",
+			vector:           "CVSS:3.1/AV:N/AC:H/PR:L/UI:R/S:U/C:L/I:L/A:L/E:P/RL:X/RC:X",
+			version:          CVSS31,
+			expectedScore:    4.4,
+			expectedSeverity: CVSS_MEDIUM,
+			expectError:      false,
 		},
 		{
 			name:        "Invalid CVSS31 vector",
@@ -59,11 +63,12 @@ func TestParseVector(t *testing.T) {
 
 		// Test cases for CVSS4
 		{
-			name:          "Valid CVSS4 vector",
-			vector:        "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:L/VA:H/SC:L/SI:L/SA:H",
-			version:       CVSS4,
-			expectedScore: 9.3,
-			expectError:   false,
+			name:             "Valid CVSS4 vector",
+			vector:           "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:H/VI:L/VA:H/SC:L/SI:L/SA:H",
+			version:          CVSS4,
+			expectedScore:    9.3,
+			expectedSeverity: CVSS_CRITICAL,
+			expectError:      false,
 		},
 		{
 			name:        "Invalid CVSS4 vector",
@@ -83,7 +88,7 @@ func TestParseVector(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			score, err := ParseVector(tc.vector, tc.version)
+			score, severity, err := ParseVector(tc.vector, tc.version)
 
 			if tc.expectError {
 				if err == nil {
@@ -95,6 +100,9 @@ func TestParseVector(t *testing.T) {
 				}
 				if score != tc.expectedScore {
 					t.Errorf("Expected score %v but got %v", tc.expectedScore, score)
+				}
+				if severity != tc.expectedSeverity {
+					t.Errorf("Expected severity %v but got %v", tc.expectedSeverity, severity)
 				}
 			}
 		})
