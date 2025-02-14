25245410a2b5042bc84c829d04c2680e4d8aa0af
Alexius22 <amatucci.work@gmail.com>
2025-02-14 01:24:05 +0100

    CLEANUP: standardize quotes and formatting in color definitions and interfaces

--- web/src/interfaces/index.ts
+++ web/src/interfaces/index.ts
@@ -1,42 +1,42 @@
 export type MenuAsideItem = {
-  label: string
-  icon?: string
-  href?: string
-  target?: string
-  color?: ColorButtonKey
-  isLogout?: boolean
-  menu?: MenuAsideItem[]
-}
+  label: string;
+  icon?: string;
+  href?: string;
+  target?: string;
+  color?: ColorButtonKey;
+  isLogout?: boolean;
+  menu?: MenuAsideItem[];
+};
 
 export type MenuNavBarItem = {
-  label?: string
-  icon?: string
-  href?: string
-  target?: string
-  isDivider?: boolean
-  isLogout?: boolean
-  isDesktopNoLabel?: boolean
-  isToggleLightDark?: boolean
-  isCurrentUser?: boolean
-  menu?: MenuNavBarItem[]
-}
+  label?: string;
+  icon?: string;
+  href?: string;
+  target?: string;
+  isDivider?: boolean;
+  isLogout?: boolean;
+  isDesktopNoLabel?: boolean;
+  isToggleLightDark?: boolean;
+  isCurrentUser?: boolean;
+  menu?: MenuNavBarItem[];
+};
 
-export type ColorKey = 'white' | 'light' | 'contrast' | 'success' | 'danger' | 'warning' | 'info'
+export type ColorKey = "white" | "light" | "contrast" | "success" | "danger" | "warning" | "info";
 
 export type ColorButtonKey =
-  | 'white'
-  | 'whiteDark'
-  | 'lightDark'
-  | 'contrast'
-  | 'success'
-  | 'danger'
-  | 'warning'
-  | 'info'
-  | 'void'
+  | "white"
+  | "whiteDark"
+  | "lightDark"
+  | "contrast"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "void";
 
-export type BgKey = 'purplePink' | 'pinkRed'
+export type BgKey = "purplePink" | "pinkRed";
 
 export type UserForm = {
-  name: string
-  email: string
-}
+  name: string;
+  email: string;
+};
