4ab90946f8e7f154d272ccc935780d37440b010e
JJJJJJack <97jack@live.it>
2025-05-22 01:37:37 +0200

    fixed nginx.conf for proxy from frontend to backend APIs routes, setup vite.config proxy to work with `npm run dev` and the containerized backend

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { Field, Form, Formik } from "formik";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Buttons from "../components/Buttons";
@@ -9,6 +9,7 @@ import FormCheckRadio from "../components/Form/CheckRadio";
 import FormField from "../components/Form/Field";
 import SectionFullScreen from "../components/Section/FullScreen";
 import { getPageTitle } from "../config";
+import axios from "axios";
 
 type LoginForm = {
   login: string;
@@ -17,10 +18,26 @@ type LoginForm = {
 };
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+  const [error, setError] = useState(false);
+
   const navigate = useNavigate();
 
+  // kryvea:kryveapassword
   const handleSubmit = _ => {
-    navigate("/dashboard");
+    axios
+      .post("/api/login", {
+        username,
+        password,
+      })
+      .then(r => {
+        console.log(r);
+      })
+      .catch(e => {
+        console.log(e);
+      });
   };
 
   const initialValues: LoginForm = {
@@ -37,23 +54,31 @@ const Login = () => {
     <SectionFullScreen bg="purplePink">
       <CardBox className="w-[500px]">
         <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Form>
-            <FormField label="Login">
-              <Field name="login" />
-            </FormField>
+          <Form className="flex flex-col gap-2">
+            <div className="grid">
+              <label htmlFor="username">Username</label>
+              <Field
+                className="no-spinner w-full max-w-full rounded px-3 py-2 dark:placeholder-gray-400"
+                name="login"
+                id="username"
+                onChange={e => setUsername(e.target.value)}
+                value={username}
+              />
+            </div>
 
-            <FormField label="Password">
-              <Field name="password" type="password" />
-            </FormField>
+            <div className="grid">
+              <label htmlFor="password">Password</label>
+              <Field name="password" type="password" onChange={e => setPassword(e.target.value)} value={password} />
+            </div>
 
             <FormCheckRadio type="checkbox" label="Remember">
-              <Field type="checkbox" name="remember" />
+              <Field type="checkbox" name="remember" value={error} onChange={e => setError(e.target.checkbox)} />
             </FormCheckRadio>
 
             <Divider />
 
             <Buttons>
-              <Button type="submit" label="Login" color="info" />
+              <Button type="submit" label="Login" color="info" onClick={() => {}} />
             </Buttons>
           </Form>
         </Formik>
