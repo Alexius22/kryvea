a5a767c042faf942addce39b7f264e64b8670981
Alexius22 <amatucci.work@gmail.com>
2025-05-31 18:11:11 +0200

    Added categories management modal and import/export functionality

--- web/src/pages/Categories.tsx
+++ web/src/pages/Categories.tsx
@@ -1,10 +1,15 @@
-import { mdiPencil, mdiPlus, mdiTabSearch, mdiTrashCan } from "@mdi/js";
+import { mdiCogs, mdiDeleteAlert, mdiDownload, mdiPencil, mdiPlus, mdiTabSearch, mdiTrashCan } from "@mdi/js";
+import { Field, Form, Formik } from "formik";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import Button from "../components/Button";
 import Buttons from "../components/Buttons";
 import CardBox from "../components/CardBox";
 import CardBoxModal from "../components/CardBox/Modal";
+import Divider from "../components/Divider";
+import FormCheckRadio from "../components/Form/CheckRadio";
+import FormCheckRadioGroup from "../components/Form/CheckRadioGroup";
+import FormField from "../components/Form/Field";
 import SectionTitleLineWithButton from "../components/Section/TitleLineWithButton";
 import Table from "../components/Table/Table";
 import { getPageTitle } from "../config";
@@ -16,9 +21,16 @@ const Categories = () => {
   const loading = false;
   const error = false;
 
+  const [isModalManageActive, setIsModalManageActive] = useState(false);
   const [isModalTrashActive, setIsModalTrashActive] = useState(false);
+  const [isModalTrashAllActive, setIsModalTrashAllActive] = useState(false);
   const handleModalAction = () => {
+    setIsModalManageActive(false);
     setIsModalTrashActive(false);
+    setIsModalTrashAllActive(false);
+  };
+  const handleModalTrashAll = () => {
+    setIsModalTrashAllActive(false);
   };
 
   useEffect(() => {
@@ -34,21 +46,85 @@ const Categories = () => {
         onConfirm={handleModalAction}
         onCancel={handleModalAction}
       >
-        <p>Are you sure to delete this vulnerability?</p>
+        <p>Are you sure you want to proceed with the deletion?</p>
         <p>
           <b>Action irreversible</b>
         </p>
       </CardBoxModal>
-
+      <CardBoxModal
+        title="Categories management"
+        buttonColor="info"
+        buttonLabel="Confirm"
+        isActive={isModalManageActive}
+        onConfirm={handleModalAction}
+        onCancel={handleModalAction}
+      >
+        <Formik initialValues={{}} onSubmit={undefined}>
+          <Form>
+            <Buttons>
+              <Button
+                icon={mdiDownload}
+                label="Export categories"
+                roundedFull
+                small
+                color="contrast"
+                onClick={() => navigate("")}
+              />
+              <Button
+                icon={mdiDeleteAlert}
+                label="Delete all categories"
+                roundedFull
+                small
+                color="danger"
+                onClick={() => setIsModalTrashAllActive(true)}
+              />
+            </Buttons>
+            <Divider />
+            <FormField label="Import categories" icons={[]}>
+              <input
+                className="input-focus max-w-96 rounded dark:bg-slate-800"
+                type="file"
+                name="json"
+                accept=".json"
+              />
+            </FormField>
+            <FormCheckRadioGroup>
+              <FormCheckRadio type="checkbox" label="Override existing categories">
+                <Field type="checkbox" name="override" />
+              </FormCheckRadio>
+            </FormCheckRadioGroup>
+          </Form>
+        </Formik>
+      </CardBoxModal>
+      <CardBoxModal
+        title="Please confirm: action irreversible"
+        buttonColor="danger"
+        buttonLabel="Confirm"
+        isActive={isModalTrashAllActive}
+        onConfirm={handleModalAction}
+        onCancel={handleModalTrashAll}
+      >
+        <p>Are you sure you want to proceed with the deletion?</p>
+      </CardBoxModal>
       <SectionTitleLineWithButton icon={mdiTabSearch} title="Categories">
-        <Button
-          icon={mdiPlus}
-          label="New category"
-          roundedFull
-          small
-          color="contrast"
-          onClick={() => navigate("/manage_category")}
-        />
+        <Buttons>
+          <Button
+            icon={mdiPlus}
+            label="New category"
+            roundedFull
+            small
+            color="contrast"
+            onClick={() => navigate("/manage_category")}
+          />
+          <Button
+            icon={mdiCogs}
+            label="Categories management"
+            roundedFull
+            small
+            color="contrast"
+            onClick={() => setIsModalManageActive(true)}
+          />
+        </Buttons>
       </SectionTitleLineWithButton>
       <CardBox noPadding>
         {loading ? (
