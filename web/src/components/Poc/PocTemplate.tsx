e21471d1a9e5818ed66f6acf71987ffddacccba5
JJJJJJack <97jack@live.it>
2025-06-09 19:39:39 +0200

    clean from useless div

--- web/src/components/Poc/PocTemplate.tsx
+++ web/src/components/Poc/PocTemplate.tsx
@@ -35,61 +35,59 @@ export default function PocTemplate({
   };
 
   return (
-    <div className="rounded-3xl">
-      <div className="relative flex flex-col p-6">
-        <div className="mb-4 flex items-center gap-4">
-          <h1 className="flex items-center gap-2 rounded px-2 text-xl uppercase">
-            <Icon path={icon} size={25} />
-            {title}
-          </h1>
-          <Button type="danger" small icon={mdiDelete} onClick={onRemovePoc(currentIndex)} />
-        </div>
-        <div className="flex flex-col gap-3">
-          <div className="flex gap-6">
-            <div className="col-span-1 col-start-12 grid">
-              <label htmlFor={positionInputId}>Position</label>
-              <input
-                className="input h-8 w-[55px] rounded text-center"
-                id={positionInputId}
-                type="number"
-                value={tmpPosition}
-                min={0}
-                max={pocList.length - 1}
-                onChange={e => setTmpPosition(+e.target.value)}
-                onKeyDown={handleKeyDown}
-                onBlur={handleBlur}
-              />
-            </div>
+    <div className="relative flex flex-col rounded-3xl p-6">
+      <div className="mb-4 flex items-center gap-4">
+        <h1 className="flex items-center gap-2 rounded px-2 text-xl uppercase">
+          <Icon path={icon} size={25} />
+          {title}
+        </h1>
+        <Button type="danger" small icon={mdiDelete} onClick={onRemovePoc(currentIndex)} />
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="flex gap-6">
+          <div className="col-span-1 col-start-12 grid">
+            <label htmlFor={positionInputId}>Position</label>
+            <input
+              className="input h-8 w-[55px] rounded text-center"
+              id={positionInputId}
+              type="number"
+              value={tmpPosition}
+              min={0}
+              max={pocList.length - 1}
+              onChange={e => setTmpPosition(+e.target.value)}
+              onKeyDown={handleKeyDown}
+              onBlur={handleBlur}
+            />
+          </div>
 
+          <div>
             <div>
-              <div>
-                <span>&nbsp;</span>
-                <div className="flex h-full gap-2">
-                  <Button
-                    text="Move Up"
-                    small
-                    className="h-8"
-                    disabled={currentIndex === 0}
-                    onClick={() =>
-                      onPositionChange(currentIndex)({ target: { value: currentIndex <= 0 ? 0 : currentIndex - 1 } })
-                    }
-                  />
-                  <Button
-                    text="Move Down"
-                    small
-                    disabled={currentIndex === pocList.length - 1}
-                    onClick={() =>
-                      onPositionChange(currentIndex)({
-                        target: { value: currentIndex >= pocList.length - 1 ? pocList.length - 1 : currentIndex + 1 },
-                      })
-                    }
-                  />
-                </div>
+              <span>&nbsp;</span>
+              <div className="flex h-full gap-2">
+                <Button
+                  text="Move Up"
+                  small
+                  className="h-8"
+                  disabled={currentIndex === 0}
+                  onClick={() =>
+                    onPositionChange(currentIndex)({ target: { value: currentIndex <= 0 ? 0 : currentIndex - 1 } })
+                  }
+                />
+                <Button
+                  text="Move Down"
+                  small
+                  disabled={currentIndex === pocList.length - 1}
+                  onClick={() =>
+                    onPositionChange(currentIndex)({
+                      target: { value: currentIndex >= pocList.length - 1 ? pocList.length - 1 : currentIndex + 1 },
+                    })
+                  }
+                />
               </div>
             </div>
           </div>
-          {children}
         </div>
+        {children}
       </div>
     </div>
   );
