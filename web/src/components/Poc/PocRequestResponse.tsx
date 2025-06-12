6d3a0610b779a0e52c5ac6ef553c3854a87b52a5
JJJJJJack <97jack@live.it>
2025-06-12 02:25:02 +0200

    request/response orientation is now responsive to viewport width

--- web/src/components/Poc/PocRequestResponse.tsx
+++ web/src/components/Poc/PocRequestResponse.tsx
@@ -64,7 +64,7 @@ export default function PocRequestResponse({
         />
       </div>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 gap-4 2xl:grid-cols-2">
         <div className="grid">
           <label htmlFor={requestTextareaId}>Request</label>
           <textarea
