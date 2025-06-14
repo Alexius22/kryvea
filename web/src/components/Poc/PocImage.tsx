8c37c27436bdf9c4b9f78d0f0509f194716bd0e7
JJJJJJack <97jack@live.it>
2025-06-14 15:08:32 +0200

    add responsive style on file upload

--- web/src/components/Poc/PocImage.tsx
+++ web/src/components/Poc/PocImage.tsx
@@ -149,7 +149,7 @@ export default function PocImage({
         </label>
         <div className="flex gap-4">
           <label
-            className="flex h-12 w-1/2 min-w-40 items-center gap-2 overflow-hidden rounded-lg bg-[color:--bg-quaternary] p-2"
+            className="clickable flex h-12 w-1/2 min-w-40 cursor-pointer items-center gap-2 overflow-hidden rounded-lg bg-[color:--bg-quaternary] p-2"
             htmlFor={imageInputId}
           >
             <span className="shrink-0 text-nowrap rounded-md border border-[color:--border] bg-[color:--bg-tertiary] px-[6px] py-[1px]">
