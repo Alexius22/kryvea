45131d5cfb6a80e1877ee923a9fe56727e2ae95b
Alexius22 <amatucci.work@gmail.com>
2025-06-15 21:38:59 +0200

    Added border on Sidebar component

--- web/src/layouts/LayoutAuthenticated.tsx
+++ web/src/layouts/LayoutAuthenticated.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router";
 import FooterBar from "../components/FooterBar";
 import NavBar from "../components/NavBar";
-import Breadcrumb from "./Breadcrumb";
 import Sidebar from "../components/Sidebar/Sidebar";
+import Breadcrumb from "./Breadcrumb";
 
 export default function LayoutAuthenticated() {
   return (
     <div className="layout-root">
-      <Sidebar className="layout-sidebar" />
+      <Sidebar className="layout-sidebar border border-[color:--border]" />
       <div className="layout-content">
         <NavBar>
           <Breadcrumb homeElement={"Home"} separator={<span> {">"} </span>} capitalizeLinks />
