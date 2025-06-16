a4222daae942a4381a23a0211736b3ab8568ccd6
JJJJJJack <97jack@live.it>
2025-06-17 00:18:15 +0200

    immoral arcane wizardy

--- web/src/components/Form/Label.tsx
+++ web/src/components/Form/Label.tsx
@@ -1,7 +1,9 @@
 export default function Label({ className = "", text, htmlFor = undefined }) {
-  return (
+  return htmlFor ? (
     <label className={`font-bold ${className}`} htmlFor={htmlFor}>
       {text}
     </label>
+  ) : (
+    <span className={`font-bold ${className}`}>{text}</span>
   );
 }
