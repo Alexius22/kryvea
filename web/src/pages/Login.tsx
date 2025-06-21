b99129342c3e46475aebbe9ba4cdb8fd1b390750
Alexius22 <amatucci.work@gmail.com>
2025-06-21 14:54:56 +0200

    Updated the login page by adding an error field and toast, and also aligned the new exported APIs

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,79 +1,66 @@
-import axios from "axios";
-import { Form, Formik } from "formik";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { postData } from "../api/api";
 import Card from "../components/CardBox/Card";
 import Grid from "../components/Composition/Grid";
+import Subtitle from "../components/Composition/Subtitle";
 import Divider from "../components/Divider";
 import Button from "../components/Form/Button";
 import Checkbox from "../components/Form/Checkbox";
 import Input from "../components/Form/Input";
 import { getPageTitle } from "../config";
 
-type LoginForm = {
-  login: string;
-  password: string;
-  remember: boolean;
-};
-
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
 
   const navigate = useNavigate();
-
-  const handleSubmit = () => {
-    axios
-      .post("/api/login", {
-        username,
-        password,
-      })
-      .then(r => {
-        console.log(r);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-  const initialValues: LoginForm = {
-    login: "TestUser",
-    password: "secretpassword",
-    remember: true,
-  };
 
   useEffect(() => {
     document.title = getPageTitle("Login");
   }, []);
 
+  const handleSubmit = () => {
+    postData(
+      "/api/login",
+      { username, password, remember },
+      () => navigate("/dashboard"),
+      err => {
+        const errorMessage = err.response.data.error;
+        setError(errorMessage);
+        toast.error(errorMessage);
+      }
+    );
+  };
+
   return (
     <div className="card-modal fixed flex min-h-screen w-screen items-center justify-center">
       <Card className="glasscard">
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-          <Form>
-            <Grid className="gap-4 p-1">
-              <Input
-                type="text"
-                id="username"
-                label="Username"
-                onChange={e => setUsername(e.target.value)}
-                value={username}
-              />
-              <Input
-                type="password"
-                id="password"
-                label="Password"
-                onChange={e => setPassword(e.target.value)}
-                value={password}
-              />
-              <Checkbox id={"remember_me"} onChange={e => setRemember} htmlFor={"remember_me"} label={"Remember me"} />
-              <Divider />
-              <Button text="Login" className="justify-center" onClick={() => {}} />
-            </Grid>
-          </Form>
-        </Formik>
+        <Grid className="gap-4 p-1">
+          <Input
+            type="text"
+            id="username"
+            label="Username"
+            onChange={e => setUsername(e.target.value)}
+            value={username}
+          />
+          <Input
+            type="password"
+            id="password"
+            label="Password"
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+          />
+          <Grid className="gap-2">
+            <Checkbox id={"remember_me"} onChange={e => setRemember} htmlFor={"remember_me"} label={"Remember me"} />
+            <Subtitle className="text-[color:--error]" text={error} />
+            <Divider />
+            <Button text="Login" className="justify-center" onClick={handleSubmit} />
+          </Grid>
+        </Grid>
       </Card>
     </div>
   );
