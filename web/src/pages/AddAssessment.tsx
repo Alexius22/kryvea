e44101599c3952b4850b555d6e3ea50cab2645c9
Alexius22 <amatucci.work@gmail.com>
2025-06-21 18:10:32 +0200

    Added missing options on AddAssessment page

--- web/src/pages/AddAssessment.tsx
+++ web/src/pages/AddAssessment.tsx
@@ -1,5 +1,4 @@
 import { mdiPlus } from "@mdi/js";
-import { Form, Formik } from "formik";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import Card from "../components/CardBox/Card";
@@ -21,87 +20,83 @@ export default function AddAssessment() {
   return (
     <div>
       <Card>
-        <Formik initialValues={{}} onSubmit={undefined}>
-          <Form>
-            <Grid>
-              <SelectWrapper
-                label="Assessment Type"
-                id="assessment_type"
-                options={[
-                  { value: "vapt", label: "Vulnerability Assessment Penetration Test" },
-                  { value: "wapt", label: "Web Application Penetration Test" },
-                  { value: "mapt", label: "Mobile Application Penetration Test" },
-                  { value: "npt", label: "Network Penetration Test" },
-                  { value: "rt", label: "Red Team" },
-                ]}
-                closeMenuOnSelect
-                onChange={option => console.log("Selected assessment type:", option.value)}
-              />
-              <Input type="text" label="Name" id="name" placeholder="Insert a name" />
-              <Grid className="grid-cols-2 gap-4">
-                <Input type="date" label="Activity period" id="start" />
-                <Input type="date" id="end" />
-              </Grid>
+        <Grid>
+          <SelectWrapper
+            label="Assessment Type"
+            id="assessment_type"
+            options={[
+              { value: "vapt", label: "Vulnerability Assessment Penetration Test" },
+              { value: "wapt", label: "Web Application Penetration Test" },
+              { value: "mapt", label: "Mobile Application Penetration Test" },
+              { value: "npt", label: "Network Penetration Test" },
+              { value: "rt", label: "Red Team" },
+            ]}
+            closeMenuOnSelect
+            onChange={option => console.log("Selected assessment type:", option.value)}
+          />
+          <Input type="text" label="Name" id="name" placeholder="Insert a name" />
+          <Grid className="grid-cols-2 gap-4">
+            <Input type="date" label="Activity period" id="start" />
+            <Input type="date" id="end" />
+          </Grid>
 
-              <Grid className="grid-cols-[1fr_auto]">
-                <Input type="text" label="Session targets" id="targets" placeholder="Insert a target" />
-                <Button className="h-12" icon={mdiPlus} text="Add Host" onClick={() => navigate("/add_host")} />
-              </Grid>
+          <Grid className="grid-cols-[1fr_auto]">
+            <Input type="text" label="Session targets" id="targets" placeholder="Insert a target" />
+            <Button className="h-[42px]" icon={mdiPlus} text="Add Host" onClick={() => navigate("/add_host")} />
+          </Grid>
 
-              <SelectWrapper
-                label="CVSS Version"
-                id="cvss_version"
-                options={[
-                  { value: "4", label: "4" },
-                  { value: "3.1", label: "3.1" },
-                  { value: "2", label: "2" },
-                ]}
-                closeMenuOnSelect
-                onChange={option => console.log("Selected CVSS version:", option.value)}
-              />
+          <SelectWrapper
+            label="CVSS Version"
+            id="cvss_version"
+            options={[
+              { value: "3.1", label: "3.1" },
+              { value: "4", label: "4" },
+            ]}
+            closeMenuOnSelect
+            onChange={option => console.log("Selected CVSS version:", option.value)}
+          />
 
-              <SelectWrapper
-                label="Environment"
-                id="environment"
-                options={[
-                  { value: "pre", label: "Pre-Production" },
-                  { value: "prod", label: "Production" },
-                ]}
-                closeMenuOnSelect
-                onChange={option => console.log("Selected environment:", option.value)}
-              />
+          <SelectWrapper
+            label="Environment"
+            id="environment"
+            options={[
+              { value: "pre", label: "Pre-Production" },
+              { value: "prod", label: "Production" },
+            ]}
+            closeMenuOnSelect
+            onChange={option => console.log("Selected environment:", option.value)}
+          />
 
-              <SelectWrapper
-                label="Testing type"
-                id="testing_type"
-                options={[
-                  { value: "white", label: "White Box" },
-                  { value: "gray", label: "Gray Box" },
-                  { value: "black", label: "Black Box" },
-                ]}
-                closeMenuOnSelect
-                onChange={option => console.log("Selected testing type:", option.value)}
-              />
+          <SelectWrapper
+            label="Testing type"
+            id="testing_type"
+            options={[
+              { value: "white", label: "White Box" },
+              { value: "gray", label: "Gray Box" },
+              { value: "black", label: "Black Box" },
+            ]}
+            closeMenuOnSelect
+            onChange={option => console.log("Selected testing type:", option.value)}
+          />
 
-              <SelectWrapper
-                label="OSSTMM Vector"
-                id="osstmm_vector"
-                options={[
-                  { value: "i2i", label: "Inside to inside" },
-                  { value: "o2o", label: "Outside to outside" },
-                  { value: "o2i", label: "Outside to inside" },
-                ]}
-                closeMenuOnSelect
-                onChange={option => console.log("Selected OSSTMM vector:", option.value)}
-              />
-              <Divider />
-              <Buttons>
-                <Button text="Submit" onClick={() => {}} />
-                <Button type="outline-only" text="Cancel" onClick={() => {}} />
-              </Buttons>
-            </Grid>
-          </Form>
-        </Formik>
+          <SelectWrapper
+            label="OSSTMM Vector"
+            id="osstmm_vector"
+            options={[
+              { value: "i2i", label: "Inside to inside" },
+              { value: "i2o", label: "Inside to outside" },
+              { value: "o2o", label: "Outside to outside" },
+              { value: "o2i", label: "Outside to inside" },
+            ]}
+            closeMenuOnSelect
+            onChange={option => console.log("Selected OSSTMM vector:", option.value)}
+          />
+          <Divider />
+          <Buttons>
+            <Button text="Submit" onClick={() => {}} />
+            <Button type="outline-only" text="Cancel" onClick={() => {}} />
+          </Buttons>
+        </Grid>
       </Card>
     </div>
   );
