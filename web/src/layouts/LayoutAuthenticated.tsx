94416bf43e08167c65e5beff0589826a13715126
JJJJJJack <97jack@live.it>
2025-07-09 01:57:23 +0200

    implement fullscreen layout support in LayoutAuthenticated component

--- web/src/layouts/LayoutAuthenticated.tsx
+++ web/src/layouts/LayoutAuthenticated.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { Outlet } from "react-router";
+import { GlobalContext } from "../App";
 import FooterBar from "../components/FooterBar";
 import NavBar from "../components/NavBar";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Breadcrumb from "./Breadcrumb";
 
 export default function LayoutAuthenticated() {
+  const {
+    useFullscreen: [fullscreen],
+  } = useContext(GlobalContext);
   return (
-    <div className="layout-root">
+    <div className={`layout-root ${fullscreen ? "layout-fullscreen" : ""}`}>
       <Sidebar className="layout-sidebar border border-[color:--border-secondary]" />
       <div className="layout-content">
         <NavBar>
