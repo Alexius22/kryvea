53c6afd338c98eeb863035b6a9967ff6684b03d1
JJJJJJack <97jack@live.it>
2025-07-14 11:56:46 +0200

    HOTFIX: refresh token only after reaching half expire time

--- app/internal/api/middleware.go
+++ app/internal/api/middleware.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"log"
 	"strings"
 	"time"
 
+	"github.com/Alexius22/kryvea/internal/mongo"
 	"github.com/Alexius22/kryvea/internal/util"
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,6 +18,7 @@ func (d *Driver) SessionMiddleware(c *fiber.Ctx) error {
 	session := c.Cookies("kryvea")
 	token, err := util.ParseUUID(session)
 	if err != nil {
+		log.Println("Invalid session token:", err)
 		util.ClearCookies(c)
 		c.Status(fiber.StatusUnauthorized)
 		return c.JSON(fiber.Map{
@@ -23,8 +26,9 @@ func (d *Driver) SessionMiddleware(c *fiber.Ctx) error {
 		})
 	}
 
-	user, err := d.mongo.User().GetByToken(token)
-	if err != nil || user.TokenExpiry.Before(time.Now()) || (!user.DisabledAt.IsZero() && user.DisabledAt.Before(time.Now())) {
+	mongoUser, err := d.mongo.User().GetByToken(token)
+	if err != nil || mongoUser.TokenExpiry.Before(time.Now()) || (!mongoUser.DisabledAt.IsZero() && mongoUser.DisabledAt.Before(time.Now())) {
+		log.Println("Invalid or expired session for user:", "Error:", err)
 		util.ClearCookies(c)
 		c.Status(fiber.StatusUnauthorized)
 		return c.JSON(fiber.Map{
@@ -32,16 +36,19 @@ func (d *Driver) SessionMiddleware(c *fiber.Ctx) error {
 		})
 	}
 
-	newToken, expires, err := d.mongo.User().RefreshUserToken(user.ID)
-	if err != nil {
-		c.Status(fiber.StatusInternalServerError)
-		return c.JSON(fiber.Map{
-			"error": "Failed to refresh session",
-		})
+	if time.Until(mongoUser.TokenExpiry) < (mongo.TOKEN_EXPIRE_TIME/2)*time.Hour {
+		newToken, expires, err := d.mongo.User().RefreshUserToken(mongoUser.ID)
+		if err != nil {
+			log.Println("Failed to refresh session for user:", mongoUser.ID, "Error:", err)
+			c.Status(fiber.StatusInternalServerError)
+			return c.JSON(fiber.Map{
+				"error": "Failed to refresh session",
+			})
+		}
+		util.SetSessionCookie(c, newToken, expires)
 	}
-	util.SetSessionCookie(c, newToken, expires)
 
-	c.Locals("user", user)
+	c.Locals("user", mongoUser)
 
 	return c.Next()
 }
@@ -49,6 +56,7 @@ func (d *Driver) SessionMiddleware(c *fiber.Ctx) error {
 func (d *Driver) ContentTypeMiddleware(c *fiber.Ctx) error {
 	if strings.Contains(c.Path(), "/upload/") && c.Method() == fiber.MethodPost {
 		if !strings.HasPrefix(c.Get("Content-Type"), "multipart/form-data") {
+			log.Println("Invalid Content-Type for upload:", c.Get("Content-Type"))
 			c.Status(fiber.StatusBadRequest)
 			return c.JSON(fiber.Map{
 				"error": "Content-Type must be multipart/form-data",
@@ -59,6 +67,7 @@ func (d *Driver) ContentTypeMiddleware(c *fiber.Ctx) error {
 
 	if (c.Method() == fiber.MethodPost || c.Method() == fiber.MethodPatch) && c.Request().Header.ContentLength() > 0 {
 		if c.Get("Content-Type") != "application/json" {
+			log.Println("Invalid Content-Type for JSON request:", c.Get("Content-Type"))
 			c.Status(fiber.StatusBadRequest)
 			return c.JSON(fiber.Map{
 				"error": "Content-Type must be application/json",
