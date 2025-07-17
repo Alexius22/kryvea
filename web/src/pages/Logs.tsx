c75b97f2673dee1984456cd6ff75a5d1732d1962
Alexius22 <amatucci.work@gmail.com>
2025-07-18 01:25:31 +0200

    Added Logs page

---
+++ web/src/pages/Logs.tsx
@@ -0,0 +1,30 @@
+import { mdiListBox } from "@mdi/js";
+import { useEffect, useState } from "react";
+import { getData } from "../api/api";
+import SectionTitleLineWithButton from "../components/Section/SectionTitleLineWithButton";
+import Table from "../components/Table";
+import { getPageTitle } from "../config";
+
+export default function Logs() {
+  const [logs, setLogs] = useState<string[]>([]);
+
+  useEffect(() => {
+    document.title = getPageTitle("Logs");
+    getData("/api/logs/search?query=", setLogs);
+  }, []);
+
+  return (
+    <div>
+      <SectionTitleLineWithButton icon={mdiListBox} title="Logs Search" />
+      <Table
+        data={logs?.map(log => ({
+          Timestamp: log,
+          Source: log,
+          Severity: log,
+          Message: log,
+        }))}
+        perPageCustom={50}
+      />
+    </div>
+  );
+}
