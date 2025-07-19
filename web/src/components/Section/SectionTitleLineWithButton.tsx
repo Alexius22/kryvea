231ad66a95f467e448ec2418a1d7811df2deae6b
JJJJJJack <97jack@live.it>
2025-07-19 16:56:21 +0200

    sticky buttons to the right and scrollable title to not expand layout-content

--- web/src/components/Section/SectionTitleLineWithButton.tsx
+++ web/src/components/Section/SectionTitleLineWithButton.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
+import { scrollElementHorizontally } from "../../hooks/useEffectCurries";
 import Icon from "../Icon";
 
 type Props = {
@@ -9,14 +10,18 @@ type Props = {
 };
 
 export default function SectionTitleLineWithButton({ icon, title, main = false, children }: Props) {
+  const titleDiv = useRef<HTMLDivElement>(null);
+
+  useEffect(scrollElementHorizontally(titleDiv), []);
+
   return (
     <section className={`${main ? "" : "py-2"} mb-2 flex items-center justify-between`}>
-      <div className="flex items-center justify-start">
+      <div ref={titleDiv} className="flex items-center justify-start overflow-x-auto">
         {icon && main && <Icon path={icon} className="mr-3" />}
         {icon && !main && <Icon path={icon} className="mr-2" size="20" />}
         <h1 className={`leading-tight ${main ? "text-3xl" : "text-2xl"}`}>{title}</h1>
       </div>
-      {children}
+      <div className="sticky right-0 bg-[color:--bg-primary]">{children}</div>
     </section>
   );
 }
