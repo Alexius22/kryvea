218d02d27a8f0b277afcee3a96ccad1e80affcbe
Alexius22 <amatucci.work@gmail.com>
2025-07-30 23:28:04 +0200

    ADD: Moved ASSESSMENT_TYPE to common.types

--- web/src/pages/AssessmentUpsert.tsx
+++ web/src/pages/AssessmentUpsert.tsx
@@ -17,17 +17,7 @@ import Label from "../components/Form/Label";
 import SelectWrapper from "../components/Form/SelectWrapper";
 import { SelectOption } from "../components/Form/SelectWrapper.types";
 import { getPageTitle } from "../config";
-import { Assessment, Target } from "../types/common.types";
-
-const ASSESSMENT_TYPE: SelectOption[] = [
-  { value: "VAPT", label: "Vulnerability Assessment Penetration Test" },
-  { value: "WAPT", label: "Web Application Penetration Test" },
-  { value: "API PT", label: "API Penetration Test" },
-  { value: "MAPT", label: "Mobile Application Penetration Test" },
-  { value: "NPT", label: "Network Penetration Test" },
-  { value: "RT", label: "Red Team Assessment" },
-  { value: "IoT", label: "IoT Device Penetration Test" },
-];
+import { Assessment, ASSESSMENT_TYPE, Target } from "../types/common.types";
 
 const CVSS_VERSIONS: SelectOption[] = [
   { value: "3.1", label: "3.1" },
