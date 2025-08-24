07a83c748ddc0a060da4a5ad8ad93ef2763e112a
Alexius22 <amatucci.work@gmail.com>
2025-08-24 22:36:55 +0200

    ADD: Added a title to the page and adjusted the color and size of the download button

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,4 +1,3 @@
-export const containerMaxW = "xl:max-w-auto xl:mx-auto";
 export const appTitle = "Kryvea";
 export const getPageTitle = (currentPageTitle: string) => `${currentPageTitle} — ${appTitle}`;
 
