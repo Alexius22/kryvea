aab8291a714171cf7b49f0cd597d2d5d69bbb20a
CharminDoge <danielechianella@hotmail.it>
2025-09-03 00:01:53 +0200

    updated populate script to create more appropriate categories and targets

--- scripts/populate_db/populate_test.py
+++ scripts/populate_db/populate_test.py
@@ -82,8 +82,8 @@ def create_categories(n: int) -> list:
     """
 
     categories = []
-    for _ in range(n):
-        category = Category()
+    for index in utils.rand_category_indexes(n):
+        category = Category(index=index)
         id, error = category.add()
         if error != "":
             raise Exception(f"Failed to register category {category.name}: {error}")
