37de2990102b51e8c8b0b74bf6b8f358621315bc
Alexius22 <amatucci.work@gmail.com>
2025-09-06 03:57:26 +0200

    EDIT: Removed padding in the footer of the Card component

--- web/src/components/Composition/Card.tsx
+++ web/src/components/Composition/Card.tsx
@@ -12,7 +12,7 @@ export default function Card({ className, children, footer, noHighlight }: Props
   return (
     <Grid className={`cardbox ${noHighlight ? `no-highlight` : ""} ${className}`}>
       {children}
-      {footer && <div className="p-6">{footer}</div>}
+      {footer && <div className="mt-4 justify-self-end">{footer}</div>}
     </Grid>
   );
 }
