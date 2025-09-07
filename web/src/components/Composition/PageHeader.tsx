a308ae0e07b835ecaeb57049587fdeb3f4a39725
JJJJJJack <97jack@live.it>
2025-09-07 02:04:09 +0200

    give space and breath around titles and move items down the title height instead of center

--- web/src/components/Composition/PageHeader.tsx
+++ web/src/components/Composition/PageHeader.tsx
@@ -10,11 +10,11 @@ type Props = {
 
 export default function PageHeader({ icon, title, main = false, children }: Props) {
   return (
-    <section className={`${main ? "" : "py-2"} mb-2 flex items-center justify-between`}>
+    <section className="mb-6 mt-2 flex items-end justify-between">
       <div className="flex items-center justify-start overflow-x-auto">
         {icon && main && <Icon path={icon} className="mr-3" />}
         {icon && !main && <Icon path={icon} className="mr-2" size="20" />}
-        <h1 className={`leading-tight ${main ? "text-3xl" : "text-2xl"}`}>{title}</h1>
+        <h1 className={`${main ? "text-3xl" : "text-2xl"}`}>{title}</h1>
       </div>
       <div className="sticky right-0 bg-[color:--bg-primary]">{children}</div>
     </section>
