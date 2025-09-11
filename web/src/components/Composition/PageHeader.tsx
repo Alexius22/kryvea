d38f8fbbbcabfd1fabc9a80498609d7117d6cf8b
Alexius22 <amatucci.work@gmail.com>
2025-09-11 20:12:40 +0200

    FIX: Fixed the title in the PageHeader which was scrolling unnecessarily

--- web/src/components/Composition/PageHeader.tsx
+++ web/src/components/Composition/PageHeader.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function PageHeader({ icon, title, main = false, children }: Props) {
   return (
     <section className="mb-6 mt-2 flex items-end justify-between">
-      <div className="flex items-center justify-start overflow-x-auto">
+      <div className="flex items-center justify-start">
         {icon && main && <Icon path={icon} className="mr-3" />}
         {icon && !main && <Icon path={icon} className="mr-2" size="20" />}
         <h1 className={`${main ? "text-3xl" : "text-2xl"}`}>{title}</h1>
