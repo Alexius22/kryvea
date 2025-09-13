b0e959ca76ff4bd804ed5cd3da2aa28006111001
CharminDoge <danielechianella@hotmail.it>
2025-09-13 03:13:32 +0200

    first attempt to make a function to split highlighted text

--- app/internal/report/data/prepare.go
+++ app/internal/report/data/prepare.go
@@ -2,6 +2,7 @@ package reportdata
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Alexius22/kryvea/internal/cvss"
 	"github.com/Alexius22/kryvea/internal/mongo"
@@ -149,4 +150,118 @@ func getOWASPCounter(vulnerabilities []mongo.Vulnerability, maxVersion string) m
 	fmt.Printf("owaspCounter: %+v\n", owaspCounter)
 
 	return owaspCounter
+}
+
+func parseHighlights(vulnerabilities []mongo.Vulnerability) {
+	for i := range vulnerabilities {
+		for j := range vulnerabilities[i].Poc.Pocs {
+			parseHighlightedText(&vulnerabilities[i].Poc.Pocs[j])
+		}
+	}
+}
+
+func parseHighlightedText(pocitem *mongo.PocItem) {
+	if pocitem.RequestHighlights != nil {
+		pocitem.RequestHighlighted = splitText(pocitem.Request, pocitem.RequestHighlights)
+	}
+	if pocitem.ResponseHighlights != nil {
+		pocitem.ResponseHighlighted = splitText(pocitem.Response, pocitem.ResponseHighlights)
+	}
+	if pocitem.TextHighlights != nil {
+		pocitem.TextHighlighted = splitText(pocitem.TextData, pocitem.TextHighlights)
+	}
+}
+
+func splitText(s string, coordinates []mongo.HighlightedText) []mongo.Highlighted {
+	if len(coordinates) == 0 {
+		return []mongo.Highlighted{
+			{
+				Text:  s,
+				Color: "",
+			},
+		}
+	}
+
+	rows := strings.Split(s, "\n")
+	colors := make([][]string, len(rows))
+	for i := range colors {
+		colors[i] = make([]string, len(rows[i]))
+		for j := range len(rows[i]) {
+			colors[i][j] = ""
+		}
+	}
+
+	modified := true
+	for modified {
+		for i := 0; i < len(coordinates); i++ {
+			modified = false
+			if coordinates[i].Start.Line > len(rows) {
+				copy(coordinates[i:], coordinates[i+1:])
+				continue
+			}
+			if coordinates[i].End.Line > len(rows) {
+				coordinates[i].End.Line = len(rows)
+				coordinates[i].End.Col = len(rows[coordinates[i].End.Line-1])
+			}
+			if coordinates[i].Start.Line != coordinates[i].End.Line {
+				coordinates = append(coordinates, mongo.HighlightedText{})
+				first, second := coordinates[i], coordinates[i]
+
+				first.End.Line--
+				first.End.Col = len(rows[first.End.Line])
+
+				second.Start.Line++
+				second.Start.Col = 1
+
+				copy(coordinates[i+2:], coordinates[i+1:])
+				coordinates[i] = first
+				coordinates[i+1] = second
+				modified = true
+			}
+			if coordinates[i].Start.Col > len(rows[coordinates[i].Start.Line-1]) {
+				coordinates[i].Start.Col = len(rows[coordinates[i].Start.Line-1])
+			}
+			if coordinates[i].Start.Col < 0 {
+				coordinates[i].Start.Col = 1
+			}
+			if coordinates[i].End.Col > len(rows[coordinates[i].End.Line-1]) {
+				coordinates[i].End.Col = len(rows[coordinates[i].End.Line-1])
+			}
+			if coordinates[i].End.Col < 0 {
+				coordinates[i].End.Col = 1
+			}
+		}
+	}
+
+	for _, coordinate := range coordinates {
+		for i := coordinate.Start.Col; i <= coordinate.End.Col; i++ {
+			colors[coordinate.Start.Line-1][i-1] = coordinate.Color
+		}
+	}
+
+	splitted := []mongo.Highlighted{}
+	splitColor := mongo.Highlighted{
+		Text:  "",
+		Color: "",
+	}
+
+	builder := strings.Builder{}
+	for i, colorRow := range colors {
+		for j, color := range colorRow {
+			if color != splitColor.Color {
+				splitColor.Text = builder.String()
+				splitted = append(splitted, splitColor)
+				splitColor = mongo.Highlighted{}
+				builder = strings.Builder{}
+			}
+			builder.WriteByte(rows[i][j])
+			splitColor.Color = color
+		}
+	}
+	if builder.Len() > 0 {
+		splitColor.Text = builder.String()
+		splitted = append(splitted, splitColor)
+	}
+
+	return splitted
 }
