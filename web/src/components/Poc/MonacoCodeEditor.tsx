c765d9cf8074b7d68d950da0ef431e6ec559d875
Alexius22 <amatucci.work@gmail.com>
2025-10-01 01:13:36 +0200

    UPDATE: Optimized build time removing unused imports

--- web/src/components/Poc/MonacoCodeEditor.tsx
+++ web/src/components/Poc/MonacoCodeEditor.tsx
@@ -1,6 +1,5 @@
 import Editor, { Monaco, OnMount } from "@monaco-editor/react";
 import type * as monaco from "monaco-editor";
-import { editor as monacoEditor } from "monaco-editor";
 import { useEffect, useRef, useState } from "react";
 import Grid from "../Composition/Grid";
 import Label from "../Form/Label";
@@ -78,7 +77,7 @@ export default function MonacoCodeEditor({
         range: new monacoRef.current!.Range(start.line, start.col, end.line, end.col),
         options: {
           inlineClassName: getOrCreateHighlightClass(color),
-          stickiness: monacoEditor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+          stickiness: monacoRef.current?.editor.TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
         },
       }));
 
