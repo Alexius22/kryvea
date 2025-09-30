778a0d4dcb467f97f0b93527d4d4f5ff085b8574
CharminDoge <danielechianella@hotmail.it>
2025-10-01 00:47:00 +0200

    fixed overlooked issue in vulnerability sorting during export, thanks to dzonerzy and Alexius22

--- app/internal/report/data/sanitize.go
+++ app/internal/report/data/sanitize.go
@@ -52,26 +52,26 @@ func SanitizeAndSortVulnerabilities(vulnerabilities []mongo.Vulnerability, maxVe
 		addComplexityColor(&vulnerabilities[i])
 
 		addSeverityColor(&vulnerabilities[i])
+	}
 
-		// Sort by maxVersion score
-		switch maxVersion {
-		case cvss.Cvss2:
-			sort.Slice(vulnerabilities, func(j, k int) bool {
-				return vulnerabilities[j].CVSSv2.Score > vulnerabilities[k].CVSSv2.Score
-			})
-		case cvss.Cvss3:
-			sort.Slice(vulnerabilities, func(j, k int) bool {
-				return vulnerabilities[j].CVSSv3.Score > vulnerabilities[k].CVSSv3.Score
-			})
-		case cvss.Cvss31:
-			sort.Slice(vulnerabilities, func(j, k int) bool {
-				return vulnerabilities[j].CVSSv31.Score > vulnerabilities[k].CVSSv31.Score
-			})
-		case cvss.Cvss4:
-			sort.Slice(vulnerabilities, func(j, k int) bool {
-				return vulnerabilities[j].CVSSv4.Score > vulnerabilities[k].CVSSv4.Score
-			})
-		}
+	// Sort by maxVersion score
+	switch maxVersion {
+	case cvss.Cvss2:
+		sort.Slice(vulnerabilities, func(j, k int) bool {
+			return vulnerabilities[j].CVSSv2.Score > vulnerabilities[k].CVSSv2.Score
+		})
+	case cvss.Cvss3:
+		sort.Slice(vulnerabilities, func(j, k int) bool {
+			return vulnerabilities[j].CVSSv3.Score > vulnerabilities[k].CVSSv3.Score
+		})
+	case cvss.Cvss31:
+		sort.Slice(vulnerabilities, func(j, k int) bool {
+			return vulnerabilities[j].CVSSv31.Score > vulnerabilities[k].CVSSv31.Score
+		})
+	case cvss.Cvss4:
+		sort.Slice(vulnerabilities, func(j, k int) bool {
+			return vulnerabilities[j].CVSSv4.Score > vulnerabilities[k].CVSSv4.Score
+		})
 	}
 }
 
